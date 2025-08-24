fbab9a7ce7a4160e48a84d77bc50fac346fbd663
Muh80 <39484022+Muh80@users.noreply.github.com>
2025-08-24 08:02:31 +0000

    Day6

---
+++ Src/Day6/fun_socket.py
@@ -0,0 +1,8 @@
+import re, socket
+pat = re.compile(r"(UNION SELECT|<script>|xp_cmdshell|/etc/passwd|Failed password|SURICATA|ET )", re.I)
+s = socket.create_connection(("127.0.0.1", 5500))
+with s, s.makefile() as f:
+    for line in f:
+        line=line.rstrip("\n")
+        hit = "  <-- SUSPICIOUS" if pat.search(line) else ""
+        print(line+hit)
